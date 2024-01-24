1ec9308c7a8b45f713dfd2e69b5bb69e2412c3a3
NotSoPr17 <89939541+NotSoPr17@users.noreply.github.com>
2024-01-23 19:44:54 -0500

    Arreglos pequeños

--- Fangame/Atla/Systems/VotingStart.lua
+++ Fangame/Atla/Systems/VotingStart.lua
@@ -1,18 +1,29 @@
 
 local MapVoteUi = [[7323242840771770665]]
-function Secondfunc()
+
+local UiOpen = false
+
+function edgardo()
+    
     threadpool:work(function() --Multi threading function
     
         for i=1,math.huge do 
         
-            local result, IsGameStarted = VarLib2:getGlobalVarByName(5,"Game_Started")
+            local result, IsVoteStarted = VarLib2:getGlobalVarByName(5,"Vote_Started")
         
             if result == ErrorCode.OK then 
-                if IsGameStarted then 
+                
+                if IsVoteStarted and not UiOpen then
+                    
+                    UiOpen = true
                     local _,_,as = World:getAllPlayers(-1)
+                    
                     for i,v in ipairs(as) do
                         Player:openUIView(v, MapVoteUi)
                     end
+                    
+                    return
+                    
                 end
             
             else
@@ -24,11 +35,11 @@ function Secondfunc()
         threadpool:wait(1)
         end
     
-    Chat:sendSystemMsg("A bug has been found, reseting the map in 5 seconds, sorry for the inconvenience!", 0)
+    Chat:sendSystemMsg("A bug has been found when initializing the map vote, reseting the map in 5 seconds, sorry for the inconvenience!", 0)
     threadpool:wait(5)
     Game:doGameEnd()
     
     end)
 end
 
-ScriptSupportEvent:registerEvent([=[Game.Start]=], Secondfunc)
+ScriptSupportEvent:registerEvent([=[Game.Start]=], edgardo)
