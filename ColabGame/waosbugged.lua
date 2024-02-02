41a8610235651bffe47c4c2a86df308008e2c048
NotSoPr17 <89939541+NotSoPr17@users.noreply.github.com>
2024-02-02 11:45:44 -0500

    Waos fixed (i think)

--- ColabGame/waosbugged.lua
+++ ColabGame/waosbugged.lua
@@ -1,7 +1,9 @@
 local Rooms = {
     Count = 0,
     RoomPlantilla = {
+        Status = nil, --"False or nil is inactive" true is active
         CurPlayers = {},
+        MaxPlayers = 20,
         x, y, z = nil,
         Data = {
             RoundsPassed = 0,       
@@ -11,50 +13,83 @@ local Rooms = {
     },
 }
 
-local function AddRooms()
+local function AddRooms(RoomNum)
     Rooms.Count = Rooms.Count + 1
     print("Room added at position: " .. Rooms.Count)
     local NewRoom = setmetatable({}, {__index = Rooms.RoomPlantilla})
     rawset(Rooms, "Room" .. Rooms.Count, NewRoom)
+    NewRoom.Status = true
     return NewRoom
 end
 
 local function RemoveRoom(index)
-    print("Room removed at index: " .. index .. "\nCurrent number of rooms: " .. Rooms.Count)
-    table.remove(Rooms, index)
+    
+    if type(index) ~= number then 
+      print("Index is not a number")
+      return
+    end
+    
+    if index == nil or index < 0 then 
+      print("Index is nil or less than 0")
+      return
+    end
+    
+    if index > Rooms.Count then 
+    
+      print("Room does not exist, number is greater than count")
+      return
+    end
+    
+    local k = 0
+        
+    for i=1,Rooms.Count do 
+        local RoomIndex = i - (i - 1) + k
+        local CurRoom = Rooms["Room".. RoomIndex]
+        
+        if RoomIndex == index then 
+            CurRoom.Status = nil
+            return
+        end
+        k = k + 1
+    end
+    
+    --[[sucess, status = pcall(table.remove, Rooms, index)
+    
+    if not sucess then
+      print("\n(Remove room function)")
+      print("Error found: ".. "\n".. status .. "\n")
+    end]]--
+    
 end
 
 local function RoomListDisplay()
       
       local k = 0
       
-      for i=2,Rooms.Count do
+      for i=1,Rooms.Count do
         
         local RoomIndex = i - (i - 1) + k
-        if Rooms["Room".. RoomIndex] then 
+        
+        local CurRoom = Rooms["Room".. RoomIndex]
+        
+        if CurRoom and CurRoom.Status then 
             print("Room: ".. RoomIndex)
         end
         k = k + 1
       end
   
   end
-
+--Scriptsupportregisterevent
 PlayerArr = {1, 2}
 
 if #PlayerArr >= 2 then
     if Rooms.Count <= 1 then
+      
         print("Game was created/ no available rooms")
         for i=1, 4 do AddRooms() end
-        RemoveRoom(1)  -- Cambiado a 2 para eliminar la segunda habitación
+        RemoveRoom(2)  -- Cambiado a 2 para eliminar la segunda habitación
         RoomListDisplay()
-        --[[sucess, errormsg = pcall(RoomListDisplay)
-        
-        if not sucess then 
-          
-          print("Error found: ")
-          print(errormsg)
-        
-        end]]--
+
         
     end
         
