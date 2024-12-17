5ed6a6e342b308da1a1be3937ecb1da0d9216d31
NotSoPr17 <89939541+NotSoPr17@users.noreply.github.com>
2024-12-16 19:23:37 -0500

    Create CustomSwords.lua
    This script uses object-oriented programming (OOP) to create a system for managing custom swords in a game. Each sword has properties such as base damage, maximum damage, and upgrade increments, allowing them to be upgraded to increase their damage. Players can own multiple swords, which can be used to attack others, reducing their health.

---
+++ CustomSwords/CustomSwords.lua
@@ -0,0 +1,117 @@
+
+local swordlist = {
+    
+    {id = 4097, dmg = 30, maxdmg = 100, incrementdmg = 10}
+    
+}
+
+local sword = {}
+local playerobj = {}
+
+function sword:new(o)
+    o = o or {}
+    
+    setmetatable(o, self)
+    self.__index = self
+    
+    self.playerid = o.playerid -- owner
+    self.itemid = o.itemid
+    
+    self.dmg = o.dmg
+    self.maxdmg = o.maxdmg
+    self.incrementdmg = o.incrementdmg
+    
+    return o 
+end
+
+function sword:addDmg(quantity) 
+    
+    print("old dmg: " .. self.dmg)
+    
+    self.dmg = math.min(self.dmg + (quantity or self.incrementdmg), self.maxdmg)
+    
+    print(" -> " .. self.dmg)
+end
+
+function sword:hurt(hurtplayerid) 
+    if Actor:isPlayer(hurtplayerid) == ErrorCode.OK then
+        
+        local _, hurtplayerhp = Player:getAttr(hurtplayerid, PLAYERATTR.CUR_HP)
+        local newhp = math.max(hurtplayerhp - self.dmg, 0)
+    
+        Player:setAttr(hurtplayerid, PLAYERATTR.CUR_HP, newhp)
+     else
+        
+        local _, hurtplayerhp = Creature:getAttr(hurtplayerid, CREATUREATTR.CUR_HP)
+        local newhp = math.max(hurtplayerhp - self.dmg, 0)
+    
+        Creature:setAttr(hurtplayerid, CREATUREATTR.CUR_HP, newhp)
+    end
+end
+
+function playerobj:new(o) 
+    o = o or {}
+    
+    setmetatable(o, self)
+    self.__index = self
+    
+    self.playerid = o.playerid -- owner
+    self.swords = {}
+    
+    print("Sword created for " .. self.playerid)
+    
+    return o 
+end
+
+function playerobj:addsword(data)
+    print("adding sword " .. data.id .. " to " .. self.playerid)
+    self.swords[data.id] = sword:new(data)
+end
+
+function playerobj:swordupgrade(id, customincrement)
+    print("increasing damage to " .. self.playerid " sword")
+    self.swords[id]:addDmg(customincrement)
+end
+
+function playerobj:swordattack(id, hurtplayerid)
+    print(self.playerid .. " hurting " .. hurtplayerid)
+    self.swords[id]:hurt(hurtplayerid)
+end
+
+local players = {}
+
+ScriptSupportEvent:registerEvent([[Game.AnyPlayer.EnterGame]], function(e) 
+
+    players[e.eventobjid] = playerobj:new({playerid = e.eventobjid}) -- add a playerobject for any new players
+    
+end) 
+
+ScriptSupportEvent:registerEvent([[Game.AnyPlayer.LeaveGame]], function(e) 
+
+    players[e.eventobjid] = nil -- delete the playerobject of any player that leaves
+    
+end) 
+
+ScriptSupportEvent:registerEvent([[Player.AttackHit]], function(e) 
+    
+    local playerid = e['eventobjid']
+    local hurtplayerid = e['toobjid'] --e['targetactorid']
+    
+    local _, curtoolid = Player:getCurToolID(playerid)
+    
+    players[playerid]:swordattack(curtoolid, hurtplayerid)
+end)
+
+ScriptSupportEvent:registerEvent([[Player.PickUpItem]], function(e) 
+    
+    local playerid = e['eventobjid']
+    local itemid = e['itemid']
+    
+    for k,v in pairs(swordlist) do 
+        if v.id == itemid then
+            v.playerid = playerid
+            players[playerid]:addsword(v)
+        end
+    end
+    
+end)
